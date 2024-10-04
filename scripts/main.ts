b794332888d653c155f4a98201aa4c4d85f62680
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-10-04 10:29:29 +0700

    feat: Setup class 'Human'

--- scripts/main.ts
+++ scripts/main.ts
@@ -11,6 +11,45 @@ interface Player {
   handleScissors(): void;
 }
 
+class Human implements Player {
+  private _name: string;
+  private _score: number = 0;
+
+  constructor(name: string, score: number) {
+    this._name = name;
+    this._score = score;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(name: string) {
+    this._name = name;
+  }
+
+  get score(): number {
+    return this._score;
+  }
+
+  set score(score: number) {
+    this._score = score;
+  }
+
+  handleRock(): void {
+      
+  }
+
+  handlePaper(): void {
+      
+  }
+
+  handleScissors(): void {
+      
+  }
+}
+
+// Generate random choice(rock, paper, or scissor)
 const getRandomChoice = (): string => {
   let choice: string;
   const randomNum = Math.random();
