9a6c89ba84ed1e235908e976433f983e7a987c8f
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-10-04 10:20:05 +0700

    feat: Implement getRandomChoice(), which returns either 'rock', 'paper' or 'scissors'

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,36 @@
 "use strict";
-console.log("Hello, rock paper scissors");
+// class Human implements Player {
+//   private _name: string;
+//   private _score: number = 0;
+//   constructor(name: string, score: number) {
+//     this._name = name;
+//     this._score = score;
+//   }
+//   get name(): string {
+//     return this._name;
+//   }
+//   set name(name: string) {
+//     this._name = name;
+//   }
+//   get score(): number {
+//     return this._score;
+//   }
+//   set score(score: number) {
+//     this._score = score;
+//   }
+// }
+// Generate random choice(rock, paper, or scissor)
+const getRandomChoice = () => {
+    let choice;
+    const randomNum = Math.random();
+    if (randomNum >= 0 && randomNum <= 0.33) {
+        choice = 'rock';
+    }
+    else if (randomNum >= 33 && randomNum <= 0.66) {
+        choice = 'paper';
+    }
+    else {
+        choice = 'scissors';
+    }
+    return choice;
+};
--- scripts/main.ts
+++ scripts/main.ts
@@ -10,3 +10,17 @@ interface Player {
   handlePaper(): void;
   handleScissors(): void;
 }
+
+const getRandomChoice = (): string => {
+  let choice: string;
+  const randomNum = Math.random();
+
+  if (randomNum >= 0 && randomNum <= 0.33) {
+    choice = 'rock';
+  } else if (randomNum >= 33 && randomNum <= 0.66) {
+    choice = 'paper';
+  } else {
+    choice = 'scissors';
+  }
+  return choice;
+};
