348ea0044519b34f944655f771a09f7e414634e2
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-10-04 09:18:29 +0700

    feat: Add 'Player' interface, which contains name, score, getName(), setName(), getScore(), setScore(), handleRock(), handlePaper(), and handleScissors()

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,1 +1,12 @@
-console.log("Hello, rock paper scissors");
+interface Player {
+  name: string;
+  score: number;
+
+  getName(): string;
+  setName(name: string): void;
+  getScore(): number;
+  setScore(score: number): void;
+  handleRock(): void;
+  handlePaper(): void;
+  handleScissors(): void;
+}
